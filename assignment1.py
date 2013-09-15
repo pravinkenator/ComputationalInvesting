6e5ef6cbf0de83f9d77c5e14f9b3e594cc6116b9
Praveen Krishnakumar <pravinkenator@gmail.com>
2013-09-15 15:31:24 +0530

    Added Comments

--- assignment1.py
+++ assignment1.py
@@ -15,26 +15,37 @@ import numpy as np
 #Assumed Trading days to be 252 as per wiki. Ideally must be len(data)
 TRADING_DAYS = 252
 
-# 
+# Function: Simulate()
+# Slight deviation from course requirement
+# Takes in the close price (numpy array and the initial allocations)
+# Does not include reading of data corresponding to the equity. This is to avoid multiple reads when optimizing
 def simulate(close_price, initial_allocations):
-    # Convert Start time and end time to date time objects
     
+    # Compute the individual equity returns corresponding to the initial allocations
     equity_returns = close_price.copy() * initial_allocations
     
+    # Compute portfolio return
     portfolio_return = np.sum(equity_returns, axis = 1)
     
+    # Compute cumulative return (the last day)
     cumulative_return = portfolio_return[-1]
     
+    # Daily return of the portfolio
     tsu.returnize0(portfolio_return)
     
+    # Compute mean, standard deviation
     port_std_dev = np.std(portfolio_return)
     port_avg_return = np.average(portfolio_return)
     
+    # Compute sharpe ratio
     sharpe_ratio = np.sqrt(TRADING_DAYS)*(port_avg_return/port_std_dev)
     
     return port_std_dev, port_avg_return, sharpe_ratio, cumulative_return
 
 
+# Function: Allocations()
+# This function returns a list of all possible allocations (which sum up to 1)
+# This is for obtaining the optimal combination for the portfolio 
 def allocations():
     all_combinations = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     combinations = []
@@ -46,11 +57,15 @@ def allocations():
                     if i + j + k + l == 1.0:
                         combinations.append([i, j, k, l])
     return combinations
+
+# Entry point for main
 if __name__ == '__main__':
 
     equity_symbols =  ['BRCM', 'TXN', 'AMD', 'ADI']  
     start_date = '2010-01-01'
     end_date = '2010-12-31'
+    
+    # Convert start, end date into date time objects
     start_date = (dt.datetime.strptime(start_date, '%Y-%m-%d')).date()
     end_date = (dt.datetime.strptime(end_date, '%Y-%m-%d')).date()
     
@@ -64,16 +79,21 @@ if __name__ == '__main__':
     tradingDays = du.getNYSEdays(start_date, end_date, timeOfDay)
     keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     
+    # Obtain data from Yahoo data source and convert to dict
     data = dataObj.get_data(tradingDays, equity_symbols, keys)
     dictData = dict(zip(keys, data))
     
     #Obtain the close values
     closeData = dictData['close'].values
+    
+    # Normalize the close values
     normalizedPrice = closeData/closeData[0,:]
         
     #Order -> sd, avg, sharp_ratio, cumulative_return
     optimal_result = [0.0, 0.0, 0.0, 0.0]
     optimal_allocation = []
+    
+    # Loop for all possible allocations and obtain the optimal one
     for allocation in allocations():
         print "Simulating Allocation: ", zip(equity_symbols, allocation)
         sd, avg, sharp_ratio, cum_ret = simulate(normalizedPrice, allocation)
